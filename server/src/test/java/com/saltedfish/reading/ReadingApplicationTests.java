508f4ec7be657b06b77cf2faeff624529c33ef74
yanbin007 <yanbin007@outlook.com>
2019-08-06 23:47:39 +0800

    初始化服务端

---
+++ server/src/test/java/com/saltedfish/reading/ReadingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.saltedfish.reading;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReadingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
